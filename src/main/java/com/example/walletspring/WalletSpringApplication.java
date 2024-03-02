f5bc4d9a79375fc7df554763808b1e0c9e4dc95f
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-02 17:32:34 +0300

    feat: initialize Wallet Spring

---
+++ src/main/java/com/example/walletspring/WalletSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSpringApplication.class, args);
+    }
+
+}
